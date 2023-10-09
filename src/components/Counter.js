dffb5100d44aa6538ec6456a89f739a4117ec997
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-10-09 15:39:30 +0500

    toogle added and working

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,8 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const counter_1 = useSelector((state) => state.counter);
+
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const decrementHandler = () => {
@@ -14,14 +16,22 @@ const Counter = () => {
     dispatch({ type: 'increment' });
     // useDispatch({ type: 'increment' });
   };
-  const toggleCounterHandler = () => {};
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', value: 20 });
+    // useDispatch({ type: 'increment' });
+  };
+  const toggleCounterHandler = (action) => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter_1}</div>
+      {show && <div className={classes.value}>{counter_1}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase By 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
